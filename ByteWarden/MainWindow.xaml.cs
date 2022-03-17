d3aff82cdf235153a2f112e86e1ced7dff128f01
Skywalk3r4 <l.rivo04@gmail.com>
2022-03-17 22:48:23 +0100

    Aggiunto grid layout e funzioni richieste dal prof TODP -> debuggare per cercare exploit

--- ByteWarden/MainWindow.xaml.cs
+++ ByteWarden/MainWindow.xaml.cs
@@ -21,22 +21,91 @@ namespace ByteWarden
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Stack<string> history = new Stack<string>();
+
         public MainWindow()
         {
             InitializeComponent();
             
         }
 
-        private static class Account
+        private void OperationBtnClick(object sender, RoutedEventArgs e)
         {
-            private static string name, password;
-            
-            public static void setName(string newName) { name = newName; }
-            public static void setPassword (string newPsw) { password = newPsw; }
+            Button btn = (Button)sender;
+            string content = btn.Content.ToString();
+            if (content == "+" || content == "-" || content == "x" || content == "/")
+            {
+                OperationLabel.Content += " " + content + " ";
+            }
+            else
+            {
+                OperationLabel.Content += content;
+            }
+        }
 
-            public static void WriteAccount()
+        private void EqualsBtnClick(object sender, RoutedEventArgs e)
+        {
+            string[] operand = OperationLabel.Content.ToString().Split(" ");
+            double result;
+            try
+            {
+                result = Convert.ToDouble(operand[0]);
+            }
+            catch (System.FormatException)
+            {
+                return;
+            }
+
+            for (int i = 1; i < operand.Length; i++)
+            {
+                try
+                {
+                    if (operand[i] == "+") result += Convert.ToDouble(operand[i + 1]);
+                    else if (operand[i] == "-") result -= Convert.ToDouble(operand[i + 1]);
+                    else if (operand[i] == "x") result *= Convert.ToDouble(operand[i + 1]);
+                    else if (operand[i] == "/") result /= Convert.ToDouble(operand[i + 1]);
+                }
+                catch (System.FormatException)
+                {
+                    return;
+                }
+            }
+            history.Push(OperationLabel.Content.ToString());
+            OperationLabel.Content = result.ToString();
+        }
+
+        private void DelBtnClick(object sender, RoutedEventArgs e)
+        {
+            string s = OperationLabel.Content.ToString();
+            try
+            {
+                if (s[s.Length - 1] == ' ')
+                    OperationLabel.Content = s.Remove(s.Length - 3);
+                else
+                    OperationLabel.Content = s.Remove(s.Length - 1);
+            }
+            catch (Exception)
             {
 
+            }
+        }
+
+        private void ACBtnClick(object sender, RoutedEventArgs e)
+        {
+            OperationLabel.Content = "";
+        }
+
+        private void UndoBtnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OperationLabel.Content = history.Pop();
+            }
+            catch (Exception)
+            {
+                history.Push("");
+                return;
             }
         }
     }
