bea8c9d3fc2b73b34ea6fd3cc1687f0e1329f8fb
12012 <12012@PC-LI-19>
2022-03-17 11:00:08 +0100

    Aggiungere i file di progetto.

---
+++ ByteWarden/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace ByteWarden
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private static class Account
+        {
+            private static string name, password;
+            
+            public static void setName(string newName) { name = newName; }
+            public static void setPassword (string newPsw) { password = newPsw; }
+
+            public static void WriteAccount()
+            {
+
+            }
+        }
+    }
+}
